eee46213f2a1f16e04bcc66faa4706c714fe22b4
silidada <596838981@qq.com>
2022-06-12 14:41:05 +0800

    添加标签读取及编码

--- face_recognition/utils/label_pre.py
+++ face_recognition/utils/label_pre.py
@@ -7,13 +7,123 @@
 
 import re
 
-def read_label(path):
+
+def encode_label(label_list: list) -> (list, list):
+    label_name = [[] for i in range(5)]
+
+    label_encoded_list = []
+
+    for label in label_list:
+        if label is None:
+            label_encoded_list.append(None)
+            continue
+        label_encoded = []
+
+        for i in range(len(label) - 1):
+            if not label[i] in label_name[i]:
+                label_name[i].append(label[i])
+                label_encoded.append(len(label_name[i]) - 1)
+            else:
+                label_encoded.append(label_name[i].index(label[i]))
+
+        if label[4] is None:
+            label_encoded.append([None])
+        else:
+            prop = []
+            for ll in label[4]:
+                if not ll in label_name[4]:
+                    label_name[4].append(ll)
+                    prop.append(len(label_name[4]) - 1)
+                else:
+                    prop.append(label_name[4].index(ll))
+            label_encoded.append(prop)
+
+        label_encoded_list.append(label_encoded)
+    return label_name, label_encoded_list
+
+
+def read_label(path: str) -> (list, list):
+    label_list = []
+    img_name = []
     with open(path, "r") as f:
         r = f.readline()
         while r:
+            img_name.append(r[:5].strip())
             t = re.search('_missing descriptor', r)
-            print(t)
+            if t is not None:
+                label_list.append(None)
+                r = f.readline()
+                continue
+            else:
+                label1 = []
+                pattern = re.compile(r'\((.*?)\)')
+                t = pattern.findall(r)
+                for s in t[0:-1]:
+                    c = s.split(" ")[-1].strip()
+                    label1.append(c)
+
+                pattern = re.compile(r'\'\((.*?)\)')
+                t = pattern.findall(r)[0].strip()
+                if not t == '':
+                    prop = []
+                    t = t.split(" ")
+                    for i in t:
+                        prop.append(i)
+                    label1.append(prop)
+                else:
+                    label1.append(None)
+            label_list.append(label1)
+            r = f.readline()
+    return label_list, img_name
+
+
+def one_hot_prop(label_encoded_list: list, prop_num: int) -> list:
+    label_list = []
+    iii = 0
+    for label in label_encoded_list:
+        iii += 1
+        if label is not None:
+            prop_one_hot = [0 for i in range(prop_num)]
+            prop = label.pop()
+            for i in prop:
+                if i is not None:
+                    prop_one_hot[i] = 1
+            label.append(prop_one_hot)
+            label_list.append(label)
+            if 1 in prop_one_hot:
+                pass
+        else:
+            label_list.append(None)
+    return label_list
+
+
+def one_hot(index, num):
+    label_one_hot = [0 for i in range(num)]
+    label_one_hot[index] = 1
+    return label_one_hot
+
+
+def one_hot_label(label_list: list, label_name) -> list:
+    label_one_hot = []
+    for label in label_list:
+        if label is not None:
+            l = []
+            for i in range(len(label_name) - 1):
+                o = one_hot(label[i], len(label_name[i]))
+                l += o
+            l += label[-1]
+        else:
+            label_one_hot.append(None)
+            continue
+        label_one_hot.append(l)
+    return label_one_hot
 
 
 if __name__ == '__main__':
-    read_label("../face/faceDR")
+    label_list, img_name = read_label("../face/faceDR")
+    label_name, label_encoded_list = encode_label(label_list)
+    prop_num = len(label_name[-1])
+    label_list = one_hot_prop(label_encoded_list, prop_num)
+    label_one_hot = one_hot_label(label_list, label_name)
+
+
